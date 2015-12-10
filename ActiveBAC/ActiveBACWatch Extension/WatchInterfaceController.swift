64712532b4162ab01dacbfa6f0a3bf1816373bd2
Student 1 <student1@csc232x16.ad.calpoly.edu>
2015-12-10 14:04:10 -0800

    fuck

---
+++ ActiveBAC/ActiveBACWatch Extension/WatchInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  WatchInterfaceController.swift
+//  ActiveBAC
+//
+//  Created by Student 1 on 12/10/15.
+//  Copyright © 2015 team[0]. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class WatchInterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
